a0f9b03aea422beb976997ed8572b1010e0b8f0d
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-09 16:23:00 +0800

    feat: add test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 tests=("InsertionSort" "PrimsList" "RingBuffer" "LRU" "LinearSearchList" "BinarySearchList" "TwoCrystalBalls" "BubbleSort" "SinglyLinkedList" "DoublyLinkedList" "Queue" "Stack" "ArrayList" "MazeSolver" "MergeSort" "QuickSort" "BTPreOrder" "BTInOrder" "BTPostOrder" "BTBFS" "DijkstraList" "CompareBinaryTrees" "DFSOnBST" "DFSGraphList" "Trie" "BFSGraphMatrix" "Map" "MinHeap")
 
-echo "${tests[@]}" | tr ' ' '\n' | fzf | xargs bun --watch test
+echo "${tests[@]}" | tr ' ' '\n' | fzf --margin 10% --border=rounded --border-label="Tests" --border-label-pos=bottom | xargs bun --watch test
